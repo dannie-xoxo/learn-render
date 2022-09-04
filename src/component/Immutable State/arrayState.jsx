23a8ccd44af9ad2cb3715ccdda133ac4cffdd414
Daniel Bryan <orisaekedanielbryan@gmail.com>
2022-09-04 13:07:29 +0100

    first commit

--- src/component/Immutable State/arrayState.jsx
+++ src/component/Immutable State/arrayState.jsx
@@ -1,3 +1,4 @@
+//State Immutability and Render
 import React, { useState } from 'react';
 
 const initState = ['Daniel', 'Bryan']
@@ -25,3 +26,10 @@ function ArrayState() {
 }
 
 export default ArrayState;
+/* State immutability and Render.
+- Mutating an object or an array as state will not cause a re-render
+when used with the useState or useReducer Hook
+- To re-render, make a copy of the exsiting state, modify as necessary 
+and then pass the new state to the new state to the setter function or
+while returning from a reducer function.
+- Directly mutating the state is an easy way to create bugs in your application. make sure you dont do that. */
