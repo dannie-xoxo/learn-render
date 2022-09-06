7709f9cfaffd377107b56ad273793792ef443766
Daniel Bryan <orisaekedanielbryan@gmail.com>
2022-09-06 18:04:05 +0100

    first commit

--- src/component/Optimization/OptChild2.jsx
+++ src/component/Optimization/OptChild2.jsx
@@ -1,3 +1,4 @@
+// React.Memo
 import React from 'react';
 
 function OptChild2() {
@@ -10,3 +11,13 @@ function OptChild2() {
 }
 export const MemoisedOptChild2 = React.memo(OptChild2)
 export default OptChild2;
+/*React.memo
+
+In React, when a parent component renders, a child component might un-necessary render
+- To optimize this behaviour, you can use react memo and pass in the child component.
+-React.memo will perform a shallow comparision of the previous and new props and
+re-render the child component only if the props have changed
+- in some cases, React.memo is used incorrectly
+- If child components have children elements(props passed as children), there's no need to wrap the component
+with react.memo because {props.children} is always a new reference which 
+will cause the child component to always re-render */   
